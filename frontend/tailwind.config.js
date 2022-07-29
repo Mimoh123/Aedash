3cf1e9c2f31d08cc1620851ba0dc050ef0cd64a6
AadarshBaral <adarsmain101@gmail.com>
2022-07-29 22:16:10 +0545

    added post_detail page, footer and varous other styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,9 +7,14 @@ module.exports = {
     extend: {
       screens: {
         'mob': {"max":"500px"},
+       'tab': {"max":"640px"},
+       'lap' : {'max' : '1024px'},
+       'desk': {"max":"1280px"},
         'scr-fix': {'min': '500px', 'max': '640px'},
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-hyphens")
+  ],
 }
